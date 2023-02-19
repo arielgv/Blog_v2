42b15eebf961c61945ade8648b6fe06a91f36d2c
Ariel Villafane <ariel.g.villafane@gmail.com>
2023-02-19 17:47:04 -0300

    sistema de busquedas funcionando

--- blog/forms.py
+++ blog/forms.py
@@ -9,3 +9,12 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+class PostFilterForm(forms.Form):
+    FILTER_CHOICES = (
+        ('newest', 'Newest'),
+        ('oldest', 'Oldest'),
+        ('most_liked', 'Most Liked'),
+    )
+    filter_by = forms.ChoiceField(choices=FILTER_CHOICES, required=False)
+    search = forms.CharField(max_length=100, required=False)
